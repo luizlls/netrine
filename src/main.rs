6b96936c94891c01d374e1137839d15c59498bad
Luiz Lopes <luizlopesdev@gmail.com>
2021-08-30 22:41:58 -0300

    basic repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+use netrine::Source;
+
+use std::path::PathBuf;
+use std::fs;
+use std::io::{stdin, stdout, Write};
+
 fn main() {
+    let args = std::env::args().collect::<Vec<String>>();
+    if args.len() <= 1 {
+        repl();
+    } else {
+        file(args.get(1).unwrap().to_string());
+    }
+}
+
+fn file(file: String) {
+    let path = PathBuf::from(file);
+    let content = fs::read_to_string(&path).expect("Couldn't open the file");
+    let source  = Source::new(&content, path);
+    exec(source);
+}
+
+fn repl() {
     println!("netrine v{}", env!("CARGO_PKG_VERSION"));
+
+    let mut input = String::new();
+
+    while let Ok(line) = read_line() {
+        if !line.is_empty() {
+            input.push_str(&line);
+            input.push('\n');
+        } else {
+            exec(Source::source(&input));
+            input.clear();
+        }
+    }
+}
+
+fn read_line() -> Result<String, ()> {
+    let mut line = String::new();
+    print!(">>>>");
+    stdout().flush().unwrap();
+    match stdin().read_line(&mut line) {
+        Ok(_) => {
+            Ok(line.trim_end().to_string())
+        }
+        _ => Err(()),
+    }
+}
+
+fn exec(source: Source) {
+    println!("{}", source.content);
 }
